4ebf623b6662f2e115ce98917214c2863d4503d7
Rahul <rahulshendre789@gmail.com>
2024-10-21 23:05:29 +0530

    css

--- server.js
+++ server.js
@@ -255,7 +255,7 @@ app.get('/logout', (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
